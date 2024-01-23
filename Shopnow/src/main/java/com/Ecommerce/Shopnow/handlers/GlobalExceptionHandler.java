5af7acef797e44614d1c6e47c06667e1ffea31dc
VictoriaAde0 <adejumotoria@gmail.com>
2024-01-23 19:49:10 +0100

    added Shopnow api

--- Shopnow/src/main/java/com/Ecommerce/Shopnow/handlers/GlobalExceptionHandler.java
+++ Shopnow/src/main/java/com/Ecommerce/Shopnow/handlers/GlobalExceptionHandler.java
@@ -1,4 +1,32 @@
 package com.Ecommerce.Shopnow.handlers;
 
+import com.Ecommerce.Shopnow.dto.CustomError;
+import com.Ecommerce.Shopnow.exceptions.RecordNotFoundException;
+import com.Ecommerce.Shopnow.exceptions.UserAlreadyExistException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 public class GlobalExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    @ExceptionHandler(UserAlreadyExistException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<CustomError> userAlreadyExist(UserAlreadyExistException ex) {
+        CustomError customError = new CustomError(HttpStatus.CONFLICT.value(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(customError);
+    }
+
+    @ExceptionHandler(RecordNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<CustomError> productNotFound(RecordNotFoundException ex) {
+        CustomError customError = new CustomError(HttpStatus.NOT_FOUND.value(),  ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(customError);
+    }
 }
